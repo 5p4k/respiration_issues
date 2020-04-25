439955af80798e7c075549e54a98cc3fd0b82609
Pietro Saccardi <lizardm4@gmail.com>
2020-04-26 01:16:00 +0200

    Cause unabridged api warning

--- src/base.h
+++ src/base.h
@@ -1,7 +1,15 @@
+#ifndef BASE_H
+#define BASE_H
+
 #include <cstdio>
 
+/**
+ * @todo This TODO directive causes <tt>(!) Unabridged API: unexpected kind 'page' (IGNORED)</tt>
+ */
 class my_base {
     int _foo;
 public:
     inline my_base(int foo) : _foo{foo} {}
 };
+
+#endif
